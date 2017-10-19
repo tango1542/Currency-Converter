c29649847856dc3396b01b2bf3cfda7d900aaa6c
Jeff Peterson <tango1542@gmail.com>
2017-10-18 20:37:22 -0500

    a few changes to make it a two way conversion

--- model/currencyDB.js
+++ model/currencyDB.js
@@ -1,4 +1,11 @@
 module.exports = {
-  EUR: 0.85,
-  JPY: 112.63
+    EtD : 1.19,
+    EtY : 133.26,
+    EtE : 1,
+    DtD : 1,
+    DtE : .84,
+    DtY : 112.4,
+    YtD : .0089,
+    YtY : 1,
+    YtE : .0075
 };
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,73 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/convert', function(req, res, next){
-  var dollars = req.query.dollars;
+  var amount = req.query.amount;
   var toCurrency = req.query.to_currency;
-  var converted = dollars * exchangeRates [toCurrency];
+  var fromCurrency = req.query.from_currency;
+  var exchRate;
+  var fromCur;
+  var toCur;
+
+if (fromCurrency === "EUR" && toCurrency === "USD") {
+  exchRate = "EtD";
+  fromCur = "Euros";
+  toCur = "Dollars";
+} else
+if (fromCurrency === "EUR" && toCurrency === "JPY") {
+    exchRate = "EtY";
+    fromCur = "Euros";
+    toCur = "Yen";
+} else
+if (fromCurrency === "EUR" && toCurrency === "EUR") {
+    exchRate = "EtE";
+    fromCur = "Euros";
+    toCur = "Euros";
+} else
+if (fromCurrency === "USD" && toCurrency === "USD") {
+    exchRate = "DtD";
+    fromCur = "Dollars";
+    toCur = "Dollars";
+} else
+if (fromCurrency === "USD" && toCurrency === "JPY") {
+    exchRate = "DtY";
+    fromCur = "Dollars";
+    toCur = "Yen";
+} else
+if (fromCurrency === "USD" && toCurrency === "EUR") {
+    exchRate = "DtE";
+    fromCur = "Dollars";
+    toCur = "Euro";
+} else
+if (fromCurrency === "JPY" && toCurrency === "USD") {
+    exchRate = "YtD";
+    fromCur = "Yen";
+    toCur = "Dollars";
+} else
+if (fromCurrency === "JPY" && toCurrency === "JPY") {
+    exchRate = "YtY";
+    fromCur = "Yen";
+    toCur = "Yen";
+} else
+if (fromCurrency === "JPY" && toCurrency === "EUR") {
+    exchRate = "YtE";
+    fromCur = "Yen";
+    toCur = "Euro";
+}
+
+var converted = amount * exchangeRates [exchRate];
+
+console.log(fromCurrency);
+console.log(toCurrency);
+console.log(amount);
+console.log(converted);
+console.log(exchRate);
+
 res.render('results', {
-  dollars: dollars,
+  amount: amount,
   toCurrency: toCurrency,
+  toCur: toCur,
+  fromCurrency: fromCurrency,
+  fromCur: fromCur,
   converted: converted}
 );
 });
